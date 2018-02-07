2d72c6feabdeef151f831817e248ab150de83392
shsibbett <36089659+shsibbett@users.noreply.github.com>
2018-02-06 22:23:33 -0500

    Making progress

--- Digital Assignment 1/js/main.js
+++ Digital Assignment 1/js/main.js
@@ -10,7 +10,7 @@ window.onload = function () {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
 
-
+    //code from phaser examples: https://phaser.io/examples/v2/p2-physics/tilemap
 
     var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render });
 
@@ -19,8 +19,9 @@ window.onload = function () {
         game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
         game.load.image('walls_1x2', 'assets/tilemaps/tiles/walls_1x2.png');
-        game.load.image('tiles2', 'assets/tilemaps/tiles/wetstone.png');
+        game.load.image('tiles2', 'assets/tilemaps/tiles/tiles2.png');
         game.load.image('ship', 'assets/sprites/thrust_ship2.png');
+        game.load.image('diamond', 'assets/sprites/diamond.png');
 
     }
 
@@ -28,12 +29,14 @@ window.onload = function () {
     var map;
     var layer;
     var cursors;
+    var diamond;
+    var text;
 
     function create() {
 
         game.physics.startSystem(Phaser.Physics.P2JS);
 
-        game.stage.backgroundColor = '#2F4F4F';
+        game.stage.backgroundColor = '#181818';
 
         map = game.add.tilemap('map');
 
@@ -57,6 +60,11 @@ window.onload = function () {
         ship = game.add.sprite(200, 200, 'ship');
         game.physics.p2.enable(ship);
 
+        diamond = game.add.sprite(1200, 400, 'diamond');
+        //        diamond.enableBody = true;
+        game.physics.p2.enable(diamond);
+        //diamond.body.onBeginContact.add(blockHit, this);
+
         game.camera.follow(ship);
 
         //  By default the ship will collide with the World bounds,
@@ -68,7 +76,7 @@ window.onload = function () {
         game.physics.p2.setBoundsToWorld(true, true, true, true, false);
 
         //  Even after the world boundary is set-up you can still toggle if the ship collides or not with this:
-        // ship.body.collideWorldBounds = false;
+        //ship.body.collideWorldBounds = false;
 
         cursors = game.input.keyboard.createCursorKeys();
 
@@ -93,8 +101,74 @@ window.onload = function () {
             ship.body.reverse(400);
         }
 
+        //if (ship.world.x = diamond.world.x - 20 || ship.world.x >= diamond.world.x + 20) {
+        //    diamond.kill();
+        //}
+
+        
+        if (ship.world.x <= 50 || ship.world.x >= 1545 || ship.world.y <= 50 || ship.world.y >= 525) {
+            // from phaser examples: https://phaser.io/examples/v2/text/text-stroke
+            text = game.add.text(game.width / 2, game.height / 2, 'You crashed!');
+            text.align = 'center';
+            text.fixedToCamera = true;
+            text.anchor.setTo(0.5, 0.5);
+
+            text.font = 'Arial Black';
+            text.fontSize = 68;
+            text.fontWeight = 'bold';
+
+            text.stroke = '#000000';
+            text.strokeThickness = 6;
+            text.fill = '#FF0000';
+
+            ship.kill();
+        }
+
     }
 
+    function collectDiamond(ship, diamond) {
+        diamond.kill();
+
+        // from phaser examples: https://phaser.io/examples/v2/text/text-stroke
+        text = game.add.text(game.width / 2, game.height / 2, 'You won!');
+        text.align = 'center';
+        text.fixedToCamera = true;
+        text.anchor.setTo(0.5, 0.5);
+
+        text.font = 'Arial Black';
+        text.fontSize = 68;
+        text.fontWeight = 'bold';
+
+        text.stroke = '#000000';
+        text.strokeThickness = 6;
+        text.fill = '#FF0000';
+    }
+
+    //function blockHit(body, bodyB, shapeA, shapeB, equation) {
+
+    //    //  The block hit something.
+    //    //  
+    //    //  This callback is sent 5 arguments:
+    //    //  
+    //    //  The Phaser.Physics.P2.Body it is in contact with. *This might be null* if the Body was created directly in the p2 world.
+    //    //  The p2.Body this Body is in contact with.
+    //    //  The Shape from this body that caused the contact.
+    //    //  The Shape from the contact body.
+    //    //  The Contact Equation data array.
+    //    //  
+    //    //  The first argument may be null or not have a sprite property, such as when you hit the world bounds.
+    //    if (body) {
+    //        bodyB.kill();
+    //        this.kill();
+    //    }
+    //    else {
+    //        body.kill();
+    //        bodyB.kill();
+    //        this.kill();
+    //    }
+
+    //}
+   
     function render() {
 
     }
